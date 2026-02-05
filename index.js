3dbf6904d7122e0b413ffd89774c0963c30b2283
thegamebeyond <gamebeyondg@gmail.com>
2026-02-05 14:07:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ client.once(Events.ClientReady, (c) => {
   console.log(`✅ Logged in as ${c.user.tag}`);
 });
 
+console.log("DISCORD_BOT_TOKEN set:", Boolean(process.env.DISCORD_BOT_TOKEN));
+console.log("DISCORD_BOT_TOKEN length:", process.env.DISCORD_BOT_TOKEN?.length ?? 0);
+
 client.login(DISCORD_BOT_TOKEN);
